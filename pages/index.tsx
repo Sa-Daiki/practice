ce0e46406d0f8a5e8c80fe73e54fbdce5faf5603
Sa-Daiki <sato-daiki@evolu.tokyo>
2022-11-07 00:05:04 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
-import ky from "ky";
 import { GetStaticProps } from "next";
 import { backendApiClient } from "../ky";
+
+const API_KEY = process.env.RESAS_API_KEY as string;
 
 const Index = (props: any) => {
   console.log(props);
   return <div>Enter</div>;
 };
 
+export default Index;
+
 export const getStaticProps: GetStaticProps = async () => {
   try {
     const data = await backendApiClient.get("prefectures", {
-      headers: { "X-API-KEY": "3xmEgnI70VZjD2Mdv72wTOA3UElpQcPEpBjh93Xp" },
+      headers: { "X-API-KEY": API_KEY },
     });
-    return { props: { data } };
+    const json = await data.json();
+    return { props: { json } };
   } catch (_) {
     console.log(_);
 
     return { notFound: true };
   }
 };
-
-export default Index;
